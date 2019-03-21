d9d3c3fde62beb36d8e08bcbba809c3af8460984
cnd0015 <cnd0015@gmail.com>
2019-03-21 08:47:28 +0700

    dieng 1.1

---
+++ BMS_Book_Management/Controllers/LoginController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BMS_Book_Management.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
