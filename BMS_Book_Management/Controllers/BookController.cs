3742db7c4f2c53d389b4c8d663a036674ba87a95
Le Thanh Dat <dattoo730@gmail.com>
2019-03-21 08:45:57 +0700

    dat

---
+++ BMS_Book_Management/Controllers/BookController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BMS_Book_Management.Controllers
+{
+    public class BookController : Controller
+    {
+        // GET: Book
+        public ActionResult Category()
+        {
+            return View();
+        }
+    }
+}
