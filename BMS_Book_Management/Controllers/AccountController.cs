93364f2363f6205827733156a591b00c8bf65b00
Le Thanh Dat <dattoo730@gmail.com>
2019-03-27 09:47:32 +0700

    ddd

---
+++ BMS_Book_Management/Controllers/AccountController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BMS_Book_Management.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult EditProfile()
+        {
+            return View();
+        }
+        public ActionResult _ChangePasswordPartial()
+        {
+            return View();
+        }
+        
+    }
+}
---
+++ BMS_Book_Management/Migrations/Configuration.cs
@@ -0,0 +1,31 @@
+namespace BMS_Book_Management.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<BMS_Book_Management.Models.AspNetIdentity.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(BMS_Book_Management.Models.AspNetIdentity.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
